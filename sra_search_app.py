53620be570a20e83b9ff404f285b7c2eeacf1f97
Hattyoriiiiiii <www.tatsuya92@gmail.com>
2024-02-08 13:53:52 +0900

    Add sample attributes to DataFrame

--- sra_search_app.py
+++ sra_search_app.py
@@ -13,9 +13,10 @@ app = Flask(__name__, template_folder='templates')
 app.config['SECRET_KEY'] = 'your_secret_key'
 download_file_name = ""
 
+
 class SearchForm(FlaskForm):
     organism = StringField('Organism', validators=[DataRequired()], default='human')
-    return_max = IntegerField('Return Max', validators=[DataRequired()], default=10000)
+    return_max = IntegerField('Return Max', validators=[DataRequired()], default=25000)
     strategy = SelectMultipleField('Strategy', 
         choices=[(strategy, strategy) for strategy in ['AMPLICON', 'ATAC-seq', 'Bisulfite-Seq', 
                  'CLONE', 'CLONEEND', 'CTS', 'ChIA-PET', 'ChIP-Seq', 'DNase-Hypersensitivity', 
@@ -28,8 +29,10 @@ class SearchForm(FlaskForm):
     query = StringField('Query')
     submit = SubmitField('Search')
 
+
 def format_link(id):
     return f'<a href="https://pubmed.ncbi.nlm.nih.gov/{id}">{id}</a>'
+
 
 def fetch_pubmed_details(pubmed_id):
     handle = Entrez.efetch(db="pubmed", id=str(pubmed_id), retmode="xml")
@@ -75,27 +78,60 @@ def update_dataframe_with_pubmed_info(df):
     df['publication_date'] = publication_dates
     return df
 
+
 def add_h3k27ac_flag(df):
     # H3K27acをターゲットにしているかどうかを示すフラグを追加
     df['h3k27ac_flag'] = df.apply(lambda row: 1 if 'ChIP-Seq' in row['experiment_library_strategy'] and 'H3K27ac' in row.get('pool_member_sample_title', '') else 0, axis=1)
     return df
 
 
+def add_sample_count_per_pubmed_id(df):
+    # 各PubMed IDごとにサンプル数を計算
+    sample_counts = df.groupby('pubmed_id').size()
+    # ChIP-seqとRNA-seqのそれぞれのさンプル数を計算
+    chip_seq_counts = df[df['experiment_library_strategy'] == 'ChIP-Seq'].groupby('pubmed_id').size()
+    rna_seq_counts = df[df['experiment_library_strategy'] == 'RNA-Seq'].groupby('pubmed_id').size()
+    
+    # 計算したサンプル数を元に新しい列'sample_count_per_pubmed_id'をDataFrameに追加
+    df['sample_count_per_pubmed_id'] = df['pubmed_id'].map(sample_counts)
+    df['chip_seq_count'] = df['pubmed_id'].map(chip_seq_counts).fillna(0).astype(int)
+    df['rna_seq_count'] = df['pubmed_id'].map(rna_seq_counts).fillna(0).astype(int)
+
+    return df
+
+
+def extract_sample_attributes(df, attribute_keywords):
+
+    for attribute_name, keywords in attribute_keywords.items():
+        attribute_values = []
+
+        for _, row in df.iterrows():
+            attribute_value = None
+            for col_name in df.columns:
+                if 'sample_attributes_' in col_name and '_tag' in col_name:
+                    tag_number = col_name.split('_')[2]  # 数値部分を取得
+                    value_col_name = f'sample_attributes_{tag_number}_value'
+                    # タグが指定されたキーワードのいずれかと一致するかチェック
+                    if pd.notna(row[col_name]) and any(keyword.lower() == row[col_name].lower() for keyword in keywords):
+                        if value_col_name in df.columns:
+                            attribute_value = row[value_col_name]
+                            break
+            attribute_values.append(attribute_value)
+
+        df[attribute_name] = attribute_values
+    return df
+
+
 def search_sra(organism, return_max, strategies, query):
     combined_df = pd.DataFrame()
 
-    # columns_to_keep = [
-    #     'study_accession', 'experiment_accession', 'sample_scientific_name',
-    #     'experiment_library_strategy', 'experiment_library_source', 'pubmed_id',
-    #     'run_1_srafile_1_date', 'experiment_library_construction_protocol',
-    #     'pool_member_sample_title', 'study_study_abstract'
-    # ]
     columns_to_keep = [
         'study_external_id_1', 'run_1_accession', 
-        'pool_member_sample_title', 'study_study_abstract', 'study_study_title',
+        'pool_member_sample_name', 'pool_member_sample_title', 'study_study_abstract', 'study_study_title',
         'pool_member_organism', 'experiment_instrument_model', 'experiment_library_strategy',
         'experiment_library_source', 'experiment_library_selection', 'library_layout',
-        'experiment_library_construction_protocol', 'study_alias', 'run_1_published', 'pubmed_id'
+        'experiment_library_construction_protocol', 'study_alias', 'run_1_published', 'pubmed_id',
+        'sample_title', 'h3k27ac_flag', 'age', 'sex', 'condition', 'cell_line'
     ]
 
     for strategy in strategies:
@@ -121,9 +157,23 @@ def search_sra(organism, return_max, strategies, query):
                     return int(pubmed_id) if pubmed_id.isdigit() else None
         return None
 
+    # PubMed IDを抽出
     combined_df['pubmed_id'] = combined_df.apply(extract_pubmed_ids, axis=1)
-    combined_df = combined_df[columns_to_keep]
+
+    # H3K27acフラグを追加
     combined_df = add_h3k27ac_flag(combined_df)
+
+    # サンプル属性情報をDataFrameに追加
+    attribute_keywords = {
+        'age': ['age', 'period', 'duration', 'developmental stage'],
+        'sex': ['sex'],
+        'condition': ['disease', 'condition'],
+        'cell_line': ['cell line', 'cell type', 'cell_line', 'cell_type']
+    }
+    combined_df = extract_sample_attributes(combined_df, attribute_keywords)
+
+    # 必要な列のみを保持
+    combined_df = combined_df[columns_to_keep]
 
     # ChIP-SeqとRNA-Seqの両方を含むPubMed IDを特定
     chip_seq_ids = set(combined_df[combined_df['experiment_library_strategy'] == 'ChIP-Seq']['pubmed_id'])
@@ -132,6 +182,9 @@ def search_sra(organism, return_max, strategies, query):
 
     # 有効なPubMed IDに一致する行を保持
     filtered_df = combined_df[combined_df['pubmed_id'].isin(valid_ids)]
+
+    # 各PubMed IDについてサンプル数を追加
+    filtered_df = add_sample_count_per_pubmed_id(filtered_df)
 
     # PubMedからのデータを取得
     filtered_df = update_dataframe_with_pubmed_info(filtered_df)
@@ -147,7 +200,7 @@ def search_sra(organism, return_max, strategies, query):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global download_file_name  # グローバル変数を使用
+    global download_file_name
 
     form = SearchForm()
     if form.validate_on_submit():
@@ -170,7 +223,7 @@ def index():
 
 @app.route('/download')
 def download():
-    global download_file_name  # グローバル変数を使用
+    global download_file_name
     return send_file(download_file_name, as_attachment=True)
 
 if __name__ == '__main__':
